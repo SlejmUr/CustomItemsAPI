04e5118b726b0f3bfa7c2764107b75cdfa9613e8
SlejmUr <slejmur@protonmail.com>
2025-05-18 00:33:28 +0200

    Setting default values to true

--- CustomItemsAPI/Config.cs
+++ CustomItemsAPI/Config.cs
@@ -20,16 +20,16 @@ internal sealed class Config
     /// <summary>
     /// Whenever getting/spawning the item should ignore the case of it.
     /// </summary>
-    public bool EasyCompare { get; set; }
+    public bool EasyCompare { get; set; } = true;
 
 
-    public bool ShowPickedUpHint { get; set; }
+    public bool ShowPickedUpHint { get; set; } = true;
 
     [Description("Hint when you picked up the custom item. {0}: DisplayName {1}: Description.")]
     public string PickedUpHint { get; set; } = "You picked up {0}\n{1}";
 
 
-    public bool ShowSelectedHint { get; set; }
+    public bool ShowSelectedHint { get; set; } = true;
 
     [Description("Hint when you selected a the custom item. {0}: DisplayName {1}: Description.")]
     public string SelectedHint { get; set; } = "You selected {0}\n{1}";
